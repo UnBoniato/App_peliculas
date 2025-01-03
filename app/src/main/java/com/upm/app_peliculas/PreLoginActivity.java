c3ca5256ea01bed2c29400a1f847f1f67da2a461
Ivan Ventura Segui <ivan.ventura@alumnos.upm.es>
2025-01-03 18:57:44 +0100

    pantalla login

--- app/src/main/java/com/upm/app_peliculas/PreLoginActivity.java
+++ app/src/main/java/com/upm/app_peliculas/PreLoginActivity.java
@@ -1,6 +1,8 @@
 package com.upm.app_peliculas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,15 @@ public class PreLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pre_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(PreLoginActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+
     }
 }
---
+++ app/src/main/java/com/upm/app_peliculas/SingUpActivity.java
@@ -0,0 +1,4 @@
+package com.upm.app_peliculas;
+
+public class SingUpActivity {
+}
